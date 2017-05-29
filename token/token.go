53abe4eee03a1e19b2bae588b756bc7e4eaa0f7f
Gideon Israel Dsouza <gidisrael@gmail.com>
2017-05-29 06:15:24 +0530

    added simple lexer

---
+++ token/token.go
@@ -0,0 +1,32 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	//idents + literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
