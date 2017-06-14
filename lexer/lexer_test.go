a4496b3eeb7fb18b3c0a4edf266aae675e96c621
Gideon Israel Dsouza <gidisrael@gmail.com>
2017-06-14 19:10:25 +0530

    Updated lexer. Attemped to lex full program.

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -3,12 +3,19 @@ package lexer
 import (
 	"testing"
 
-	"../token"
+	"github.com/gideondsouza/monkey/token"
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
-	test := []struct {
+	input := `let five = 5;
+				let ten = 10;
+
+				let add = fn(x, y) {
+				x + y;
+				};
+				let result = add(five, ten);
+				`
+	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
@@ -25,7 +32,7 @@ func TestNextToken(t *testing.T) {
 
 	l := New(input)
 
-	for i, tt = range tests {
+	for i, tt := range tests {
 		tok := l.NextToken()
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
