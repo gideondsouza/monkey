7a6c23184960c8c34fcf78b878236a920e4f1306
Gideon Dsouza <gideon.dsouza@algorisys.com>
2017-06-26 10:13:18 +0530

    Added half a parser test.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -16,5 +16,26 @@ let foobar = 838383
 	p := New(l)
 
 	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
 
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !TestLetStatment(t, stmt, tt.expectedIdentifier) {
+
+		}
+	}
 }
