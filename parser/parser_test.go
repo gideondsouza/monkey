d3fd9f97e656c84d9158a72332f6a23a95f11d1f
Gideon Dsouza <gideon.dsouza@algorisys.com>
2017-07-06 17:05:20 +0530

    Wrote non-compiling parser test for integer.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -70,6 +70,34 @@ func TestReturnStatements(t *testing.T) {
 	}
 }
 
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "5;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program should only have one statement. I got=%d",
+			len(program.Statements))
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statement[0] is not an ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+
+	literal, ok = stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.IntegerLiteral. got=%T", stmt.Expression)
+	}
+	if literal.Value != 5 {
+		t.Errorf("literal.TokenLiteral not %s. got=%s", "5",
+			literal.TokenLiteral())
+	}
+}
+
 func TestIdentifierExpression(t *testing.T) {
 	input := "foobar;"
 
